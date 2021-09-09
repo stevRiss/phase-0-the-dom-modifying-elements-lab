a8f6b04100aff8f78e284fdd3dcf6e605d224bf8
Stephen <stevriss22@gmail.com>
2021-09-09 17:03:26 -0400

    COmpleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your code here!
+// 1
+const  main = document.getElementById("main");
+main.remove();
+
+//2
+/*
+const newHeader = document.createElement("h1");
+document.body.append(newHeader);
+
+const newHead = document.getElementsByTagName('h1');
+newHead.id = 'victory';
+document.body.append(newHead);
+*/
+//3
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = '<p> Stephen is the champion! </p>';
+document.body.appendChild(newHeader);
+
+
+
+
